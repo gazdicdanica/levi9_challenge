df6de7a20014febf07f1c5abf806eac132b43e6e
Danica Gazdić <danicagazdic@gmail.com>
2023-11-14 16:59:09 +0100

    Project initialization

---
+++ src/main/java/com/example/levi9_challenge/Levi9ChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.example.levi9_challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Levi9ChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Levi9ChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/levi9_challenge/Levi9ChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.levi9_challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Levi9ChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
